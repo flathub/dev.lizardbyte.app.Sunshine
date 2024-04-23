6ab4d0778ea7c091f6578d795874c3e45e498fa8
Hugo Locurcio <hugo.locurcio@hugo.pro>
2024-04-24 00:33:42 +0200

    Allow passing CLI arguments to Sunshine launcher script (#87)
    This can be used to customize Sunshine behavior on startup.

--- data/dev.lizardbyte.app.Sunshine.sh
+++ data/dev.lizardbyte.app.Sunshine.sh
@@ -4,7 +4,7 @@ PORT=47990
 
 if ! curl -k https://localhost:$PORT > /dev/null 2>&1; then
   (sleep 3 && xdg-open https://localhost:$PORT) &
-  sunshine
+  sunshine "$@"
 else
   xdg-open https://localhost:$PORT
 fi
